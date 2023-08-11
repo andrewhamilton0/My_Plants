105147ddd7a647def51d8090c1e3315a4481e2ed
Andrew Hamilton <acjts425@gmail.com>
2023-08-11 00:49:01 -0400

    Adds more colors

--- androidApp/src/main/java/com/example/myplants/android/plant/presentation/plantlistscreen/components/AddFab.kt
+++ androidApp/src/main/java/com/example/myplants/android/plant/presentation/plantlistscreen/components/AddFab.kt
@@ -8,19 +8,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myplants.android.R
 import com.example.myplants.android.core.theme.accent500
 import com.example.myplants.android.core.theme.neutralus100
 
-@Preview
 @Composable
-fun AddFab() {
+fun AddFab(
+    onClick: () -> Unit,
+    modifier: Modifier
+) {
     IconButton(
-        onClick = { /*TODO*/ },
-        modifier = Modifier
+        onClick = onClick,
+        modifier = modifier
             .clip(RoundedCornerShape(12.dp))
             .background(
                 color = accent500
@@ -28,7 +30,7 @@ fun AddFab() {
     ) {
         Icon(
             imageVector = ImageVector.vectorResource(id = R.drawable.ic_add),
-            contentDescription = null,
+            contentDescription = stringResource(id = R.string.add_plant),
             tint = neutralus100
         )
     }
