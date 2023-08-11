a9ff472ad890b92d90efeef018213e43c7d6f907
Andrew Hamilton <acjts425@gmail.com>
2023-08-10 22:00:07 -0400

    Adds Fab, refactor old fab button to NotificationBellBtn and adds color file

--- androidApp/src/main/java/com/example/myplants/android/plant/presentation/plantlistscreen/components/AddFab.kt
+++ androidApp/src/main/java/com/example/myplants/android/plant/presentation/plantlistscreen/components/AddFab.kt
@@ -1,6 +1,7 @@
 package com.example.myplants.android.plant.presentation.plantlistscreen.components
 
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
@@ -8,24 +9,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myplants.android.R
+import com.example.myplants.android.core.theme.accent500
+import com.example.myplants.android.core.theme.neutralus100
 
-
+@Preview
 @Composable
-fun AddFab(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
+fun AddFab() {
     IconButton(
-        onClick = onClick,
-        modifier = modifier
-            .clip(CircleShape)
-        // TODO ADD COLOR MODIFIER
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .clip(RoundedCornerShape(12.dp))
+            .background(
+                color = accent500
+            )
     ) {
         Icon(
-            imageVector = ImageVector.vectorResource(R.drawable.ic_notification_bell),
-            contentDescription = null // TODO WRITE DESCRIPTION
-            // TODO ADD TINT MODIFIER
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_add),
+            contentDescription = null,
+            tint = neutralus100
         )
     }
 }
